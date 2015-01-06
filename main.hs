c222caae487cd84fb7c1dc9c367e89be3dbda95d
flyingfisch <flyingfisch@toppagedesign.com>
2015-01-05 22:20:56 -0500

    improved it

--- main.hs
+++ main.hs
@@ -1,10 +1,30 @@
+import Data.List
+
 main = do
-	putStrLn "Type some text to translate into Pig Latin!"
-	input <- getLine
-	putStrLn $ toPigLatin input
+    putStrLn "Translate (t)o or (f)rom Pig Latin?"
+    choice <- getLine
+    let translator = chooser choice
+
+    putStrLn "Type some text to translate into Pig Latin!"
+    input <- getLine
+    putStrLn $ translator input
+
+chooser :: String -> (String -> String)
+chooser choice =
+    case choice of "t" -> toPigLatin
+                   "f" -> fromPigLatin
+                   otherwise -> fromPigLatin
 
 toPigLatin :: String -> String
 toPigLatin = unwords . map wordToPigLatin . words
 
+fromPigLatin :: String -> String
+fromPigLatin = unwords . map wordFromPigLatin . words
+
 wordToPigLatin :: String -> String
-wordToPigLatin (x:xs) = xs ++ [x] ++ "nay"
+wordToPigLatin (x:xs) = xs ++ [x] ++ "ay"
+
+wordFromPigLatin :: String -> String
+wordFromPigLatin x =
+    let withoutAy = x \\ "ay"
+    in [last withoutAy] ++ init withoutAy
